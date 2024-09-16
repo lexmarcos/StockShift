f698b46d45dea08dd86eac6f323fbff519343c2f
lexmarcos <marcosuel.vieira@hotmail.com>
2024-09-16 14:26:56 -0300

    feat: add manifest json of the pwa

--- src/app/(stockshift)/inventories/page.tsx
+++ src/app/(stockshift)/inventories/page.tsx
@@ -29,18 +29,18 @@ export default function Inventories() {
   });
 
   return (
-    <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
+    <main className="flex flex-1 flex-col gap-4">
       <ModalCreateInventory
         isOpen={isModalCreateInventoryOpen}
         onClose={() => setIsModalCreateInventoryOpen(false)}
       />
-      <div className="flex justify-between">
+      <div className="flex flex-col gap-3 md:flex-row justify-between mb-8 px-3 ">
         <h1 className="font-bold text-4xl">Estoques</h1>
         <Button onClick={() => setIsModalCreateInventoryOpen(true)}>
           Criar estoque +
         </Button>
       </div>
-      <div className="grid grid-cols-4 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-4">
+      <div className="grid grid-cols-1 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 gap-4">
         {data?.map((inventory) => (
           <Card
             key={inventory.id}
