45aa524748cf4de9784a6c5146f6fac9c4f153e0
lexmarcos <marcosuel.vieira@hotmail.com>
2026-01-05 20:28:44 -0300

    refactor: update create page to use shared ProductForm component

--- app/products/create/page.tsx
+++ app/products/create/page.tsx
@@ -1,48 +1,10 @@
 "use client";
 
 import { useProductCreateModel } from "./products-create.model";
-import { ProductCreateView } from "./products-create.view";
+import { ProductForm } from "../components/product-form.view";
 
 export default function ProductCreatePage() {
-  const {
-    form,
-    onSubmit,
-    isSubmitting,
-    categories,
-    isLoadingCategories,
-    brands,
-    isLoadingBrands,
-    customAttributes,
-    addCustomAttribute,
-    removeCustomAttribute,
-    updateCustomAttribute,
-    nameInputRef,
-    openScanner,
-    closeScanner,
-    isScannerOpen,
-    handleBarcodeScan,
-    warehouseId,
-  } = useProductCreateModel();
+  const modelProps = useProductCreateModel();
 
-  return (
-    <ProductCreateView
-      form={form}
-      onSubmit={onSubmit}
-      isSubmitting={isSubmitting}
-      categories={categories}
-      isLoadingCategories={isLoadingCategories}
-      brands={brands}
-      isLoadingBrands={isLoadingBrands}
-      customAttributes={customAttributes}
-      addCustomAttribute={addCustomAttribute}
-      removeCustomAttribute={removeCustomAttribute}
-      updateCustomAttribute={updateCustomAttribute}
-      nameInputRef={nameInputRef}
-      openScanner={openScanner}
-      closeScanner={closeScanner}
-      isScannerOpen={isScannerOpen}
-      handleBarcodeScan={handleBarcodeScan}
-      warehouseId={warehouseId}
-    />
-  );
+  return <ProductForm mode="create" {...modelProps} />;
 }
